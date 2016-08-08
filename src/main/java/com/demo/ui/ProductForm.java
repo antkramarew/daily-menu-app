b3895aa4cca167bd99bc4e9a0ba3d5ab7f1a3485
anton_kramarev <anton_kramarev@epam.com>
2016-08-08 19:14:32 +0300

    MTable, popup window

---
+++ src/main/java/com/demo/ui/ProductForm.java
@@ -0,0 +1,83 @@
+package com.demo.ui;
+
+import com.demo.domain.Product;
+import com.demo.repository.ProductRepository;
+import com.vaadin.data.fieldgroup.BeanFieldGroup;
+import com.vaadin.event.ShortcutAction;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.spring.annotation.SpringComponent;
+import com.vaadin.spring.annotation.UIScope;
+import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.vaadin.viritin.MBeanFieldGroup;
+import org.vaadin.viritin.button.ConfirmButton;
+import org.vaadin.viritin.button.MButton;
+import org.vaadin.viritin.fields.MTextField;
+import org.vaadin.viritin.form.AbstractForm;
+import org.vaadin.viritin.layouts.MCssLayout;
+import org.vaadin.viritin.layouts.MFormLayout;
+import org.vaadin.viritin.layouts.MVerticalLayout;
+
+/**
+ * Created by anton_kramarev on 8/4/2016.
+ */
+@SpringComponent
+@UIScope
+public class ProductForm extends AbstractForm<Product> {
+
+    @Autowired
+    private ProductRepository repository;
+
+    private Product product;
+
+    private final MTextField name = new MTextField("Product name:");
+    private final MTextField protein = new MTextField("Protein:");
+    private final MTextField fat = new MTextField("Fat:");
+    private final MTextField carbohydrate = new MTextField("Carbohydrate:");
+    private final MTextField calories = new MTextField("Calories:");
+
+    private final MButton save = new MButton(FontAwesome.SAVE,"Save",e -> {
+        repository.save(product);
+        closePopup();
+    }).withClickShortcut(ShortcutAction.KeyCode.ENTER)
+            .withStyleName(ValoTheme.BUTTON_PRIMARY);
+
+    private final MButton cancel = new MButton("Cancel", event -> {
+        closePopup();
+    });
+
+    private final CssLayout actions = new MCssLayout(save, cancel).withStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
+    private  MFormLayout formLayout;
+
+    @Override
+    protected Component createContent() {
+
+        formLayout = new MFormLayout(this.name,
+                this.protein,
+                this.fat,
+                this.carbohydrate,
+                this.calories,
+                this.actions).withWidth("");
+        MVerticalLayout verticalLayout = new MVerticalLayout(formLayout, getToolbar()).withWidth("");
+        return verticalLayout;
+    }
+
+
+    public interface ChangeHandler {
+
+        void onChange();
+    }
+
+
+
+    @Override
+    public MBeanFieldGroup<Product> setEntity(Product entity) {
+        this.product = entity;
+        return super.setEntity(entity);
+    }
+
+    public void setChangeHandler(ChangeHandler h) {
+        save.addClickListener(e -> h.onChange());
+    }
+}
--- src/main/java/com/demo/ui/VaadinUI.java
+++ src/main/java/com/demo/ui/VaadinUI.java
@@ -2,68 +2,88 @@ package com.demo.ui;
 
 import com.demo.domain.Product;
 import com.demo.repository.ProductRepository;
-import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.annotations.Theme;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
+import org.vaadin.viritin.button.ConfirmButton;
+import org.vaadin.viritin.button.MButton;
+import org.vaadin.viritin.fields.MTable;
+import org.vaadin.viritin.layouts.MCssLayout;
 
 import java.util.Collection;
 
 /**
  * Created by anton_kramarev on 8/4/2016.
  */
+@Theme("valo")
 @SpringUI
 public class VaadinUI extends UI {
 
     @Autowired
     private ProductRepository productRepository;
     @Autowired
-    private ProductEditor productEditor;
+    private ProductForm productForm;
 
-    private final Button addNewBtn;
+    private final MTable<Product> table = new MTable(Product.class)
+            .withProperties("id", "name", "protein", "fat", "carbohydrate", "calories")
+            .withColumnHeaders("Number", "Name", "Protein", "Fat", "Carbohydrate", "Calories")
+            .withFullWidth();
 
-    private final Grid grid;
+    private final MButton add = new MButton(FontAwesome.PLUS,
+            event -> {
+                productForm.setEntity(new Product());
+                productForm.openInModalPopup();
+            });
+
+    private final MButton delete = new ConfirmButton(FontAwesome.TRASH_O,"Are you sure?",
+            event -> {
+                productRepository.delete(table.getValue().getId());
+            });
+
+    private final MButton edit = new MButton(FontAwesome.PENCIL_SQUARE_O,
+            event -> {
+                productForm.setEntity(productRepository.findOne(table.getValue().getId()));
+                productForm.openInModalPopup();
+            });
+
+    private final MCssLayout actions = new MCssLayout(add, edit, delete)
+            .withStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
+
     private final TextField filter;
 
 
     public VaadinUI() {
-        this.grid = new Grid();
-        this.addNewBtn = new Button("New product", FontAwesome.PLUS);
         this.filter = new TextField();
-
     }
+
+
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn);
-        VerticalLayout mainLayout = new VerticalLayout(actions, grid, productEditor);
-        setContent(mainLayout);
 
-        actions.setSpacing(true);
-        mainLayout.setMargin(true);
-        mainLayout.setSpacing(true);
 
-        grid.setHeight(300, Unit.PIXELS);
-        grid.setColumns("id", "name", "protein", "fat", "carbohydrate", "calories");
+        HorizontalLayout toolbar = new HorizontalLayout(filter, actions);
+        VerticalLayout productsLayout = new VerticalLayout(toolbar, table);
+        table.setBeans((Collection<Product>) productRepository.findAll());
+        table.setSelectable(true);
+        toolbar.setSpacing(true);
+        productsLayout.setMargin(true);
+        productsLayout.setSpacing(true);
+        TabSheet tabSheet = new TabSheet();
+        tabSheet.setSizeFull();
+        tabSheet.addTab(productsLayout, "Products");
+        setContent(tabSheet);
 
-        filter.setInputPrompt("Filter by last name");
+        filter.setInputPrompt("Filter by name");
         filter.addTextChangeListener(e -> listProducts(e.getText()));
 
-        grid.addSelectionListener(e -> {
-            if (e.getSelected().isEmpty()) {
-                productEditor.setVisible(false);
-            }
-            else {
-                productEditor.editProduct((Product) grid.getSelectedRow());
-            }
-        });
-
-        addNewBtn.addClickListener(e -> productEditor.editProduct(new Product("", 0,0,0,0)));
-        productEditor.setChangeHandler(() -> {
-            productEditor.setVisible(false);
+        productForm.setChangeHandler(() -> {
+            productForm.setVisible(false);
             listProducts(filter.getValue());
         });
         listProducts(null);
@@ -71,13 +91,18 @@ public class VaadinUI extends UI {
 
     private void listProducts(String text) {
         if (StringUtils.isEmpty(text)) {
-            grid.setContainerDataSource(
-                    new BeanItemContainer(Product.class, (Collection) productRepository.findAll()));
+            table.setBeans((Collection) productRepository.findAll());
         }
         else {
-            grid.setContainerDataSource(new BeanItemContainer(Product.class,
-                    productRepository.findByNameStartsWithIgnoreCase(text)));
+            table.setBeans(productRepository.findByNameStartsWithIgnoreCase(text));
         }
+        applyButtonStates();
+    }
+
+    private void applyButtonStates() {
+        boolean isSelected = table.getValue() != null;
+        edit.setEnabled(isSelected);
+        delete.setEnabled(isSelected);
     }
 
 
